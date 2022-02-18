23a558ccb7680f124ea136f1e398131c347b6233
Tagore Hari Ravindra B <jdeepika@audintel.com>
2022-02-18 15:55:27 +0530

    new code

--- src/Missing.java
+++ src/Missing.java
@@ -1,22 +1,27 @@
 public class Missing {
-    int n=108;
-    int[] arr=new int
-            [String.valueOf(n).length];
-    // By Rama
 
-    i=0;
-    while(n!=0){
-        arr[i]=(n%10);
-        n=n/10;
-        i++;}
-    for (int i =arr[arr.length-1]; i <=arr[0]; i++) {
-        Boolean found=false;
-        for (int a :arr) {
-            if (i == a) {
-                found = true;
-                break;
-            }
+    public static void main(String[] args) {
+        int n=108;
+        int[] arr=new int
+                [String.valueOf(n).length()];
+        // By Rama
+
+        int j=0;
+        while (n != 0) {
+            arr[j]=(n%10);
+            n=n/10;
+            j++;
         }
-        if(!found) System.out.print(i);
+        for (int i =arr[arr.length-1]; i <= arr[0]; i++) {
+            boolean found=false;
+            for (int a :arr) {
+                if (i == a) {
+                    found = true;
+                    break;
+                }
+            }
+            if(!found) System.out.print(i);
+        }
     }
+
 }
